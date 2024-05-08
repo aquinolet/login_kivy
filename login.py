221131632b255929246d45a2957e9e906c5b9f36
Letícia Aquino <131019654+aquinolet@users.noreply.github.com>
2024-05-08 17:07:24 -0300

    Update login.py

--- login.py
+++ login.py
@@ -4,34 +4,47 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
+from kivy.utils import get_color_from_hex
 
 class MeuApp(App):
-    def build(self):
-        tela = BoxLayout(orientation='vertical',spacing=20, padding=[300,200])
-        texto = Label(text='Login', font_size=70,size_hint=(None, None), halign='center')
-        email = TextInput(text='Digite seu E-mail')
-        senha = TextInput(text='Digite sua Senha')
-        botao_login = Button(text='Login')
-        botao_cadastro = Button(text='Cadastro')
-        botao_login.bind(on_press=self.login)
-        botao_cadastro.bind(on_press=self.cadastro)
-        imagem = Image(source='imagem.png')
+    def login(self):
+        Window = BoxLayout(orientation='vertical', padding=[120,120], spacing=11)
 
-       
-        tela.add_widget(texto)
-        tela.add_widget(email)
-        tela.add_widget(senha)
-        tela.add_widget(botao_login)
-        tela.add_widget(botao_cadastro)
-        return tela
+        #imagem
+        im = Image(source="imagem.png")
 
-    def login(instance):
-        print("Login feito com sucesso!")
+        #textos
+        texto = Label(text='LOGIN', font_size=40, font_name=('Arial'))
+        texto1 = Label(text='Usuário:', font_size=20, color=get_color_from_hex('#8576FF'))
+        texto2 = Label(text='Senha:',font_size=20, color=get_color_from_hex('#8576FF'))
 
-    def cadastro(instance):
-        print("Faça seu cadastro")
+        #caixa de inserir
+        email = TextInput(text='', background_color=get_color_from_hex('#FFF7FC'))
+        senha = TextInput(text='', background_color=get_color_from_hex('#FFF7FC'))
+
+        #botoes
+        botao_login = Button(text='Login', on_press=self.botao_l, background_color=get_color_from_hex('#8576FF'))
+        botao_cadastro = Button(text='Fazer cadastro', background_color=get_color_from_hex('#8576FF'))
+
+        #aparecer mensagem
+        self.label_mensagem = Label(color=get_color_from_hex('#8576FF'))
 
 
+        Window.add_widget(texto)
+        Window.add_widget(im)
+        Window.add_widget(texto1)
+        Window.add_widget(email)
+        Window.add_widget(texto2)
+        Window.add_widget(senha)
+        Window.add_widget(self.label_mensagem)
+        Window.add_widget(botao_login)
+        Window.add_widget(botao_cadastro)
+
+        return Window
+    
+    def botao_l(self,instance):
+        mensagem = 'Login feito com sucesso!'
+        self.label_mensagem.text = mensagem
 
 if __name__=='__main__':
     MeuApp().run()
